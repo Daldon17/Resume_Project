d690a293ab7e900db156d1f4503060ce48998bd0
Daldon17 <daldonvargis03@gmail.com>
2026-05-04 12:34:38 +0530

    3rd

--- two/settings.py
+++ two/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l4s^dfz@7%0k3@-c8f7a+*8#)3(ci8i+!8y=eglnz*)40mpt66
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'three/static')
 ]
